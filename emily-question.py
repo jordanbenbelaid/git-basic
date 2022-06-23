abf9e090d7a185e4bcd5fadb2aff9136438393a6
emmy-bradfield <emily-bradfield@outlook.com>
2022-06-23 11:49:51 +0100

    added a text file and a question

---
+++ emily-question.py
@@ -0,0 +1,4 @@
+print("If you have a saucepan full of boiling oil,\nand you take a raw egg, still in it's shell,\nand add it to the saucepan of boiling oil...")
+print("Is the egg being...\na) boiled, or\nb) fried")
+answer = input()
+print("Thanks, you said '{}'".format(answer))
